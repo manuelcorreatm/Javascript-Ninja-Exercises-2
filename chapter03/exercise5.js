3036605603232ddac858bf32f97ae1f6a9b68037
manuelcorreatm <mjcorrea@tekmexico.com>
2016-04-04 15:40:17 -0700

    Last exercise of chapter 03

--- chapter03/exercise5.js
+++ chapter03/exercise5.js
@@ -1,1 +1,26 @@
-﻿
+﻿function CustomObject(a, b) {
+    this.a = a;
+    this.b = b;
+}
+CustomObject.prototype.c = function () { return this.a + this.b; };
+
+function printObjProp(obj, inherited) {
+    if (inherited == true) {
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+                console.log(prop);
+            }
+        }
+    } else {
+        for (var prop in obj) {
+            console.log(prop);
+        }
+    }
+
+}
+
+
+var obj = new CustomObject(1, 2);
+printObjProp(obj); // output: a, b, c
+printObjProp(obj, false); // output: a, b, c
+printObjProp(obj, true); // output: a, b
