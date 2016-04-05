6cd78c5f3ddc0998b83355b85ecfc97bce3a0930
manuelcorreatm <mjcorrea@tekmexico.com>
2016-04-04 17:39:29 -0700

    Exercise 1 and 2 of chapter 4

---
+++ chapter04/exercise1.js
@@ -0,0 +1,14 @@
+﻿function fibonacci(position) {
+    if (position == 1 || position == 2) {
+        return 1;
+    } else {
+        return fibonacci(position - 1) + fibonacci(position - 2);
+    }
+}
+
+
+var n = fibonacci(4); // 3
+var m = fibonacci(9); // 34
+
+console.log(n);
+console.log(m);
---
+++ chapter04/exercise2.js
@@ -0,0 +1,13 @@
+﻿function addRec(arr) {
+    if (arr.length > 0) {
+        return arr.pop() + addRec(arr);
+    } else {
+        return 0;
+    }
+    
+}
+
+var arr = [1, 3, 5, 7];
+var sum = addRec(arr); // 16
+
+console.log(sum);
