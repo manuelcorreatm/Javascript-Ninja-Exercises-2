e6f51e4336cef215f91ea36685ed330c2da273d3
manuelcorreatm <mjcorrea@tekmexico.com>
2016-04-12 09:41:44 -0700

    Finished exercise 2 of chapter 7

--- chapter07/exercise2.js
+++ chapter07/exercise2.js
@@ -1,6 +1,26 @@
-﻿var date = new Date(Date.UTC(2012, 11, 12, 3, 0, 0));
+﻿function fromUSAtoJapanDate(americanDate) {
+    var regexp = /^([0-9]{2})\/([0-9]{2})\/([0-9]{1,4})$/i;
+    var dateArr = americanDate.match(regexp);
+    if (!dateArr) return "wrong value";
+    var japanDate = dateArr[3] + "年" + dateArr[1] + "月" + dateArr[2] + "日";
+    var holiday = holidays[parseInt(dateArr[1] + dateArr[2])];
+    return japanDate + (holiday ? "(" + holiday + ")" : "");
+}
 
-// toLocaleDateString() without arguments depends on the implementation,
-// the default locale, and the default time zone
-console.log(date.toLocaleDateString("jp"));
-// → "12/11/2012" if run in en-US locale with time zone America/Los_Angelesese
+var holidays = {
+    101: "New Year's Day",
+    211: "Foundation Day",
+    321: "Vernal Equinox Day",
+    429: "Showa Day",
+    503: "Constitution Memorial Day",
+    504: "Greenery Day",
+    505: "Children's Day",
+    811: "Mountain Day",
+    923: "Autumnal Equinox Day",
+    1103: "Culture Day",
+    1123: "Labour Thanksgiving Day",
+    1223: "The Emperor's Birthday"
+}
+
+var japanDate = fromUSAtoJapanDate("12/23/2014");
+console.log(japanDate);
