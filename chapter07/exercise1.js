5c26600040ae12959b5de0cb288a64f0aa4aa104
manuelcorreatm <mjcorrea@tekmexico.com>
2016-04-12 09:56:46 -0700

    Improved exercise 1 of chapter 7

--- chapter07/exercise1.js
+++ chapter07/exercise1.js
@@ -1,8 +1,10 @@
 ﻿function getRGB(hexColor) {
-    var regexp = /^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i;
+    //var regexp = /^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i;
+    var regexp = /[0-9a-f]{2}/ig;
     var hexArr = hexColor.match(regexp);
+    if (hexArr.length != 3) return "Error: Incorrect format"
     var rgb = "rgb(";
-    for (var i = 1; i < hexArr.length; i++) {
+    for (var i = 0; i < hexArr.length; i++) {
         rgb += parseInt(hexArr[i], 16) + ",";
     }
     return rgb.replace(/,$/, ")");
