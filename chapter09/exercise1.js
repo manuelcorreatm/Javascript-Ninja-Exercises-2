ca138450210d9b879db70754985b07899a3a12dd
manuelcorreatm <mjcorrea@tekmexico.com>
2016-04-14 14:21:12 -0700

    Finished the exercise of chapter 9

---
+++ chapter09/exercise1.js
@@ -0,0 +1,7 @@
+﻿function dataParse(str) {
+    return eval("(" + str + ")");
+}
+
+var str = "{prop1: 42, myFn: function(a, b) { return a+b+this.prop1;}}";
+var obj = dataParse(str);
+console.log(obj.myFn(1,2)); //45
