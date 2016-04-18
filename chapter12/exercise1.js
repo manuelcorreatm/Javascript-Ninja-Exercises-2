714add709139a54621253021b0b8c32167466bc4
manuelcorreatm <mjcorrea@tekmexico.com>
2016-04-18 16:19:45 -0700

    Finished exercise of chapter 12, retrieval of DOM attributes

---
+++ chapter12/exercise1.js
@@ -0,0 +1,13 @@
+﻿function printAttr(elem, attributes) {
+    for (var i = 0; i < attributes.length; i++) {
+        console.log(elem.getAttribute(attributes[i]));
+    }    
+}
+
+var el = document.getElementById("a");
+printAttr (el, ["id", "class", "style", "val"]);
+    // should print out:
+    // a
+    // square
+    // display:inline-block
+    // something important
