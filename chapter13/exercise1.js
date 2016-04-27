8c8b0ff7c401f497e7b0b51824a6bf9406581eea
manuelcorreatm <mjcorrea@tekmexico.com>
2016-04-27 15:05:46 -0700

    Finishid exercise 1 of chapter 13

---
+++ chapter13/exercise1.js
@@ -0,0 +1,9 @@
+﻿var table = document.getElementsByTagName("table")[0];
+
+table.addEventListener("click",
+    function () {
+        if (event.target.tagName.toLowerCase() == "td") {
+            alert(event.target.textContent);
+        }
+    }
+);
