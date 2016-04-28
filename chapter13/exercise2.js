2e4fdab607e8641331cf05946ada23dbc3eeee75
manuelcorreatm <mjcorrea@tekmexico.com>
2016-04-28 12:30:22 -0700

    Finished Chapter 13 exercises and Exercise 1 of Chapter 14

---
+++ chapter13/exercise2.js
@@ -0,0 +1,11 @@
+﻿var btn = document.getElementById("btn");
+
+function fn() {
+    var mtr = document.getElementById("mtr");
+    mtr.value = mtr.value + 1;
+    if (mtr.value == 3) {
+        this.removeEventListener("click", fn, false);
+    }
+}
+
+btn.addEventListener("click", fn, false);
---
+++ chapter14/exercise1.js
@@ -0,0 +1,29 @@
+﻿var form = document.getElementById("form");
+
+form.addEventListener("input", renderTable, false);
+
+function renderTable() {
+    var table = document.getElementById("table");
+    var width = this.children[0].value;
+    var height = this.children[1].value;
+    var tr, td, tbody;
+
+    while (table.firstChild) {
+        table.removeChild(table.firstChild);
+    }
+
+    tbody = document.createElement("tbody");
+    for (var i = 0; i < height; i++) {
+        tr = document.createElement("tr");
+        for (var j = 0; j < width; j++) {
+            td = document.createElement("td");
+            td.textContent = j+(i*width);
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    }
+    table.appendChild(tbody);
+        
+    
+    
+}
