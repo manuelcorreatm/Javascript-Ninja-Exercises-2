b169864f79df345f956928d453df046dc649517f
manuelcorreatm <mjcorrea@tekmexico.com>
2016-05-01 18:57:38 -0700

    Finished exercise 2 of chapter 14

--- chapter14/exercise2.js
+++ chapter14/exercise2.js
@@ -1,1 +1,19 @@
-﻿
+﻿var form = document.getElementById("form");
+
+function fn() {
+    var parag = document.getElementById("paragraph");
+
+    switch (event.target.value) {
+        case "2":
+            parag.style.columnCount = 2;
+            break;
+        case "3":
+            parag.style.columnCount = 3;
+            break;
+        case "4":
+            parag.style.columnCount = 4;
+            break;
+    }
+}
+
+form.addEventListener("click", fn, false);
